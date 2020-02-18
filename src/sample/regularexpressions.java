1045473f9944fd735c68ad93bc8f9bc186a97975
cruszz <enotrunner@gmail.com>
2020-02-18 22:05:38 +0300

    upd

---
+++ src/sample/regularexpressions.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regularexpressions {
+
+
+    public static void main(String[] args) {
+        String text = "youremail@email.com";
+        Pattern pattern = Pattern.compile("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$");
+
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            int start=matcher.start();
+            int end=matcher.end();
+
+        }
+
+        System.out.println(text);
+
+
+    }
+}
