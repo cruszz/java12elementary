a7ec10f8ea501835017c4f49d1dccdb6de6a6545
cruszz <enotrunner@gmail.com>
2020-02-18 22:04:02 +0300

    гз

--- src/sample/recursion.java
+++ src/sample/recursion.java
@@ -1,8 +1,15 @@
 package sample;
 
 public class recursion {
-    public static void main(String[] args) {
+    public static String recursion(int n) {
 
+        if (n == 1) {
+            return "1";
+        }
+        return recursion(n - 1) + " " + n;
     }
 
+    public static void main(String[] args) {
+        System.out.println(recursion(10));
+    }
 }
--- src/sample/scannerexercise.java
+++ src/sample/scannerexercise.java
@@ -5,18 +5,27 @@ public class scannerexercise {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String simbol = scanner.nextLine();
+        int sum = scanner.nextInt();
+        int sum2 = scanner.nextInt();
         String input = scanner.next();
-        int sum = 0;
-        boolean stop = false;
-        if (input.equals("stop")) {
-            stop = true;
+        int res = 0;
+      //  boolean stop = false;
+        if (input.equals("+")) {
+           // stop = true;
+            res = sum + sum2;
+        }
+        if (input.equals("-")) {
+            res = sum - sum2;
         }
         while (true) {
-            sum += Integer.parseInt(input);
+            sum = Integer.parseInt(input);
+            sum2 = Integer.parseInt(input);
+            //sum += Integer.parseInt(input);
             input = scanner.next();
             if (input.equals("stop")) {
-                stop = true;
-                System.out.println(sum);
+               // stop = true;
+                System.out.println(res);
                 break;
             }
 
