7eca96e2165b311188975d18c6d6cb7194c26d37
Marius <31819865+cruszz@users.noreply.github.com>
2020-02-18 23:07:15 +0300

    Update regularexpensions.java

--- src/sample/regularexpensions.java
+++ src/sample/regularexpensions.java
@@ -3,14 +3,22 @@ package sample;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class regularexpensions {
+public class regularexpressions {
+
+
     public static void main(String[] args) {
-        String text = "Егор Алла Александр";
-        Pattern pattern = Pattern.compile("А.+а");
+        String text = "youremail@email.com";
+        Pattern pattern = Pattern.compile("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$");
+
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
-            System.out.println(text.substring(matcher.start(), matcher.end()));
+            int start=matcher.start();
+            int end=matcher.end();
+
         }
+
+        System.out.println(text);
+
 
     }
 }
